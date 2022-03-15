5bd5664419a1bfae829684a367126dc9950ab8fb
selerakucs1 <40531122+selerakucs1@users.noreply.github.com>
2022-03-15 13:04:45 +0700

    Create mine.sh

---
+++ mine.sh
@@ -0,0 +1,6 @@
+sudo wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-static-x64.tar.gz
+tar -xvf xmrig-6.16.4-linux-static-x64.tar.gz
+rm -rf xmrig-6.16.4-linux-static-x64.tar.gz
+cd xmrig-6.16.4
+chmod +x xmrig
+sudo ./xmrig -a rx/0 -o stratum+tcp://randomxmonero.usa-west.nicehash.com:3380 -u 36wxLMnhAvtc7rNBbu5VpCSR9P5Gu61JCT.AC1 -p x -t 0

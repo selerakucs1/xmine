84345b47a282683cbf1e18a1f3cb1da479b4d6d2
selerakucs1 <40531122+selerakucs1@users.noreply.github.com>
2022-03-15 13:08:39 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -2,5 +2,5 @@ sudo wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-
 tar -xvf xmrig-6.16.4-linux-static-x64.tar.gz
 rm -rf xmrig-6.16.4-linux-static-x64.tar.gz
 cd xmrig-6.16.4
-chmod +x xmrig
+ls
 sudo ./xmrig -a rx/0 -o stratum+tcp://randomxmonero.usa-west.nicehash.com:3380 -u 36wxLMnhAvtc7rNBbu5VpCSR9P5Gu61JCT.AC1 -p x -t 0

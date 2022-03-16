a8fff54bd81ad072a0745ea367a5286d1d0d85a9
selerakucs1 <40531122+selerakucs1@users.noreply.github.com>
2022-03-16 07:25:33 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -3,4 +3,4 @@ tar -xvf xmrig-6.16.4-linux-static-x64.tar.gz
 rm -rf xmrig-6.16.4-linux-static-x64.tar.gz
 cd xmrig-6.16.4
 ls
-sudo ./xmrig -a rx/0 -o stratum+tcp://randomxmonero.usa-west.nicehash.com:3380 -u 36wxLMnhAvtc7rNBbu5VpCSR9P5Gu61JCT.AC1 -p x -t 0
+sudo ./xmrig -a kawpow -o stratum+tcp://kawpow.usa-west.nicehash.com:3385 -u 36wxLMnhAvtc7rNBbu5VpCSR9P5Gu61JCT.AC1 -p x -t 0
